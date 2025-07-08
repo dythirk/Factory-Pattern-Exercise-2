5600beb0d6defe40d2dc454ff97b9a6ddc04c4fc
Thai Vo <tahrbo@gmail.com>
2025-07-08 17:20:00 -0500

    Starting

---
+++ FactoryPatternExercise2/ListDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class ListDataAccess : IDataAccess
+    {
+        public void LoadData()
+        {
+            Console.WriteLine(“I am reading data from a list, SQL database, mongo database.");
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine(“I am reading data from a list, SQL database, mongo database.");
+        }
+    }
+}
---
+++ FactoryPatternExercise2/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class Product
+    {
+        public string Name { get; set; } = "foo";
+        public double Price { get; set; } = 25000;
+    }
+}
